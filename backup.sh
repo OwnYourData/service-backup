723fe82a598c04fadda7132145b7953601dea38f
Christoph Fabianek <christoph.fabianek@gmail.com>
2017-03-18 17:57:37 +0100

    docker wildcard handling

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 # add the following line as cronjob: crontab -e
 # 5 * * * * /home/user/oyd/service-backup/backup.sh
 while read p; do
-  docker cp $p:/archive/*.zip /home/user/oyd/service-backup/$p
+  docker cp $p:/archive /home/user/oyd/service-backup/$p
   docker cp /home/user/oyd/service-backup/$p/*.zip archive:/oyd/archives
-done <pia_list.txt
-rm -rf /home/user/oyd/service-backup/*/
+  rm -rf /home/user/oyd/service-backup/$p
+done </home/user/oyd/service-backup/pia_list.txt
